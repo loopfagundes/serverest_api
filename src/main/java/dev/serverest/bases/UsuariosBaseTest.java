0e34a8a928452f2fec115860ad73f77f89f91e57
Luciano <lof.fagundes@gmail.com>
2025-03-27 11:59:47 -0300

    adicionei baseTeste e CaseTest GET dos usuarios

--- src/main/java/dev/serverest/bases/UsuariosBaseTest.java
+++ src/main/java/dev/serverest/bases/UsuariosBaseTest.java
@@ -1,4 +1,45 @@
 package dev.serverest.bases;
 
-public class UsuariosBaseTest {
+import dev.serverest.endponits.EndpointConfig;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.apache.http.HttpStatus;
+import org.testng.annotations.BeforeClass;
+
+public class UsuariosBaseTest extends EndpointConfig {
+    public static RequestSpecification baseUrl;
+    public static RequestSpecification usuariosRequest;
+    public static ResponseSpecification statusOK;
+
+    @BeforeClass
+    public void setUp() {
+        baseUrl();
+        usuarios();
+        responseStatusOK();
+    }
+
+    private void baseUrl() {
+        baseUrl = new RequestSpecBuilder()
+                .setBaseUri(BASE_URI)
+                .setContentType(ContentType.JSON)
+                .build();
+    }
+
+    private void usuarios() {
+        usuariosRequest = new RequestSpecBuilder()
+                .setBaseUri(BASE_URI)
+                .setBasePath(PATH_USUARIOS)
+                .setContentType(ContentType.JSON)
+                .build();
+    }
+
+    private void responseStatusOK() {
+        statusOK = new ResponseSpecBuilder()
+                .expectStatusCode(HttpStatus.SC_OK)
+                .expectContentType(ContentType.JSON)
+                .build();
+    }
 }
--- src/test/java/dev/serverest/usuarios/UsuariosTestCase.java
+++ src/test/java/dev/serverest/usuarios/UsuariosTestCase.java
@@ -1,4 +1,25 @@
 package dev.serverest.usuarios;
 
-public class UsuariosTestCase {
+import dev.serverest.bases.UsuariosBaseTest;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+public class UsuariosTestCase extends UsuariosBaseTest {
+
+    @Epic("Teste de Usuarios - API")
+    @Feature("Teste o buscar todos usuarios cadastrados na lista.")
+    @Description("Deve retornar 200 e todos lista dos usuarios cadastrados.")
+    @Test
+    public void getUsuario() {
+        RestAssured.given()
+                .spec(usuariosRequest)
+                .when()
+                .get()
+                .then()
+                .log().all()
+                .spec(statusOK);
+    }
 }

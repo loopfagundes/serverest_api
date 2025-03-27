3bd4154d6ba5069bcc363745f9dcb8c5fb04e15c
Luciano <lof.fagundes@gmail.com>
2025-03-27 11:59:01 -0300

    enpoint configurado!

--- src/main/java/dev/serverest/endponits/EndpointConfig.java
+++ src/main/java/dev/serverest/endponits/EndpointConfig.java
@@ -1,4 +1,14 @@
 package dev.serverest.endponits;
 
+import dev.serverest.managers.PropertiesManager;
+
+import java.util.Properties;
+
 public class EndpointConfig {
+    private static final String DEFAULT_FILE = "configuration";
+    private static final String DEFAULT_FOLDER = "url";
+    private static final Properties properties = PropertiesManager.loadProperties(DEFAULT_FILE, DEFAULT_FOLDER);
+
+    protected static final String BASE_URI = properties.getProperty("BASE_URI");
+    protected static final String PATH_USUARIOS = properties.getProperty("PATH_USUARIOS");
 }

3bce4a6370ef324e361b68f03b3211932a92a244
Luciano <lof.fagundes@gmail.com>
2025-04-01 11:55:38 -0300

    adicionei cadastrar do usuario

--- src/main/java/dev/serverest/bases/UsuariosBaseTest.java
+++ src/main/java/dev/serverest/bases/UsuariosBaseTest.java
@@ -1,24 +1,23 @@
 package dev.serverest.bases;
 
 import dev.serverest.endponits.EndpointConfig;
+import dev.serverest.stubs.CadastrarUsuarioStub;
 import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
-import io.restassured.specification.ResponseSpecification;
-import org.apache.http.HttpStatus;
 import org.testng.annotations.BeforeClass;
 
 public class UsuariosBaseTest extends EndpointConfig {
     public static RequestSpecification baseUrl;
-    public static RequestSpecification usuariosRequest;
-    public static ResponseSpecification statusOK;
+    public static RequestSpecification pathUsuariosRequest;
+    public static RequestSpecification cadastrarUsuarioRequest;
+    protected static String idUser;
 
     @BeforeClass
     public void setUp() {
         baseUrl();
-        usuarios();
-        responseStatusOK();
+        pathUsuarios();
+        cadastrarDoUsuario();
     }
 
     private void baseUrl() {
@@ -28,18 +27,20 @@ public class UsuariosBaseTest extends EndpointConfig {
                 .build();
     }
 
-    private void usuarios() {
-        usuariosRequest = new RequestSpecBuilder()
+    private void pathUsuarios() {
+        pathUsuariosRequest = new RequestSpecBuilder()
                 .setBaseUri(BASE_URI)
                 .setBasePath(PATH_USUARIOS)
                 .setContentType(ContentType.JSON)
                 .build();
     }
 
-    private void responseStatusOK() {
-        statusOK = new ResponseSpecBuilder()
-                .expectStatusCode(HttpStatus.SC_OK)
-                .expectContentType(ContentType.JSON)
+    private void cadastrarDoUsuario() {
+        cadastrarUsuarioRequest = new RequestSpecBuilder()
+                .setBaseUri(BASE_URI)
+                .setBasePath(PATH_USUARIOS)
+                .setBody(CadastrarUsuarioStub.usuarioDto())
+                .setContentType(ContentType.JSON)
                 .build();
     }
 }

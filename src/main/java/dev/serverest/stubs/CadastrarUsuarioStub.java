6ee70eb6600a4e41c49d9ccda6193e7feb58decf
Luciano <lof.fagundes@gmail.com>
2025-04-01 11:56:29 -0300

    atualizado

--- src/main/java/dev/serverest/stubs/CadastrarUsuarioStub.java
+++ src/main/java/dev/serverest/stubs/CadastrarUsuarioStub.java
@@ -1,4 +1,15 @@
 package dev.serverest.stubs;
 
+import dev.serverest.dto.UsuarioDTO;
+
 public class CadastrarUsuarioStub {
+    private static final UsuarioDTO dto = new UsuarioDTO();
+
+    public static UsuarioDTO usuarioDto() {
+        dto.setNome("Luciano");
+        dto.setEmail("test@testng.com");
+        dto.setPassword("secret123");
+        dto.setAdministrador("true");
+        return dto;
+    }
 }

7d6127f8f42d38c55c4b4dc0ddba8471e73da4f3
Luciano <lof.fagundes@gmail.com>
2025-04-01 17:10:05 -0300

    adicionei uma classe loginStub

--- src/main/java/dev/serverest/stubs/LoginStub.java
+++ src/main/java/dev/serverest/stubs/LoginStub.java
@@ -1,4 +1,12 @@
 package dev.serverest.stubs;
 
+import dev.serverest.dto.LoginDTO;
+
 public class LoginStub {
+    public static LoginDTO loginUsuario() {
+        return new LoginDTO(
+                "test@testng.com",
+                "secret123"
+        );
+    }
 }

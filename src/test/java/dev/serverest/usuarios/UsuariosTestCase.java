ce7a88ae55050a199da031dc3fabda1542af9fc3
Luciano <lof.fagundes@gmail.com>
2025-04-01 12:00:00 -0300

    testado cadastrar um novo usuario 201 e exculir um usuario cadastrado 200

--- src/test/java/dev/serverest/usuarios/UsuariosTestCase.java
+++ src/test/java/dev/serverest/usuarios/UsuariosTestCase.java
@@ -1,10 +1,12 @@
 package dev.serverest.usuarios;
 
 import dev.serverest.bases.UsuariosBaseTest;
+import dev.serverest.managers.PropertiesManager;
 import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.restassured.RestAssured;
+import org.apache.http.HttpStatus;
 import org.testng.annotations.Test;
 
 public class UsuariosTestCase extends UsuariosBaseTest {
@@ -13,13 +15,44 @@ public class UsuariosTestCase extends UsuariosBaseTest {
     @Feature("[GET] - Teste para buscar todos os usuários cadastrados na lista.")
     @Description("Deve retornar 200 e a lista completa de usuários cadastrados.")
     @Test
-    public void getTodosOsUsuarioCadastradosNaLista200() {
+    public void getBuscarTodosOsUsuarioCadastradosNaLista200() {
         RestAssured.given()
-                    .spec(usuariosRequest)
+                    .spec(pathUsuariosRequest)
                 .when()
                     .get()
                 .then()
+                    .statusCode(HttpStatus.SC_OK)
+                    .log().all();
+    }
+
+    @Epic("Teste de Usuarios")
+    @Feature("[POST] - Teste para fazer cadastrar um novo usuário")
+    @Description("Deve retornar 200 e usuário cadastrado.")
+    @Test
+    public void postCadastrarUmNovoUsuario201() {
+       idUser =
+        RestAssured.given()
+                    .spec(cadastrarUsuarioRequest)
+                .when()
+                    .post()
+                .then()
+                    .statusCode(HttpStatus.SC_CREATED)
                     .log().all()
-                    .spec(statusOK);
+                .extract().path("_id");
+        PropertiesManager.setProperty("authorize", "id", "ID_USER", idUser);
+    }
+
+    @Epic("Teste de Usuarios")
+    @Feature("[DELETE] - Teste para excluir um usuário cadastrado.")
+    @Description("Deve retornar 200 e confirmar que o usuário foi deletado.")
+    @Test
+    public void deleteUsuario200() {
+        RestAssured.given()
+                    .spec(pathUsuariosRequest)
+                .when()
+                    .delete("/" + ID_USER)
+                .then()
+                    .statusCode(HttpStatus.SC_OK)
+                    .log().all();
     }
 }

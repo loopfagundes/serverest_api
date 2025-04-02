aafa01306d36a9fd67bd68e15875720e9548b95a
Luciano <lof.fagundes@gmail.com>
2025-04-02 17:58:54 -0300

    mudei os metodos para ordem e adicionei uma priority no dentro da anotação, para definir a ordem de execucao dos testes.

--- src/test/java/dev/serverest/testcases/usuarios/UsuariosTestCase.java
+++ src/test/java/dev/serverest/testcases/usuarios/UsuariosTestCase.java
@@ -1,4 +1,4 @@
-package dev.serverest.usuarios;
+package dev.serverest.testcases.usuarios;
 
 import dev.serverest.bases.UsuariosBaseTest;
 import dev.serverest.managers.PropertiesManager;
@@ -12,9 +12,23 @@ import org.testng.annotations.Test;
 public class UsuariosTestCase extends UsuariosBaseTest {
 
     @Epic("Teste de Usuarios")
+    @Feature("[DELETE] - Teste para excluir um usuário cadastrado.")
+    @Description("Deve retornar 200 e confirmar que o usuário foi deletado.")
+    @Test(priority = 1)
+    public void deleteUsuario200() {
+        RestAssured.given()
+                    .spec(pathUsuariosRequest)
+                .when()
+                    .delete("/" + ID_USER)
+                .then()
+                    .statusCode(HttpStatus.SC_OK)
+                    .log().all();
+    }
+
+    @Epic("Teste de Usuarios")
     @Feature("[GET] - Teste para buscar todos os usuários cadastrados na lista.")
     @Description("Deve retornar 200 e a lista completa de usuários cadastrados.")
-    @Test
+    @Test(priority = 2)
     public void getBuscarTodosOsUsuarioCadastradosNaLista200() {
         RestAssured.given()
                     .spec(pathUsuariosRequest)
@@ -26,23 +40,9 @@ public class UsuariosTestCase extends UsuariosBaseTest {
     }
 
     @Epic("Teste de Usuarios")
-    @Feature("[GET] - Teste para buscar por ID um usuário cadastrado na lista.")
-    @Description("Deve retornar 200 e a lista do ID um usuário cadastrado.")
-    @Test
-    public void getBuscarUmUsuarioCadastradoNaLista200() {
-        RestAssured.given()
-                    .spec(pathUsuariosRequest)
-                .when()
-                    .get("/" + ID_USER)
-                .then()
-                    .statusCode(HttpStatus.SC_OK)
-                    .log().all();
-    }
-
-    @Epic("Teste de Usuarios")
     @Feature("[POST] - Teste para fazer cadastrar um novo usuário")
-    @Description("Deve retornar 200 e usuário cadastrado.")
-    @Test
+    @Description("Deve retornar 201 e usuário cadastrado.")
+    @Test(priority = 3)
     public void postCadastrarUmNovoUsuario201() {
         idUser =
                 RestAssured.given()
@@ -57,28 +57,28 @@ public class UsuariosTestCase extends UsuariosBaseTest {
     }
 
     @Epic("Teste de Usuarios")
-    @Feature("[PUT] - Teste para fazer editar um usuário cadastrado")
-    @Description("Deve retornar 200 e um usuário cadastrado foi editado.")
-    @Test
-    public void putEditarUmUsuario200() {
+    @Feature("[GET] - Teste para buscar por ID um usuário cadastrado na lista.")
+    @Description("Deve retornar 200 e a lista do ID um usuário cadastrado.")
+    @Test(priority = 4)
+    public void getBuscarPorIdDoUsuarioCadastradoNaLista200() {
         RestAssured.given()
-                    .spec(editarUsuarioRequest)
+                    .spec(pathUsuariosRequest)
                 .when()
-                    .put("/" + ID_USER)
+                    .get("/" + idUser)
                 .then()
                     .statusCode(HttpStatus.SC_OK)
                     .log().all();
     }
 
     @Epic("Teste de Usuarios")
-    @Feature("[DELETE] - Teste para excluir um usuário cadastrado.")
-    @Description("Deve retornar 200 e confirmar que o usuário foi deletado.")
-    @Test
-    public void deleteUsuario200() {
+    @Feature("[PUT] - Teste para fazer editar um usuário cadastrado")
+    @Description("Deve retornar 200 e um usuário cadastrado foi editado.")
+    @Test(priority = 5)
+    public void putEditarUmUsuario200() {
         RestAssured.given()
-                    .spec(pathUsuariosRequest)
+                    .spec(editarUsuarioRequest)
                 .when()
-                    .delete("/" + ID_USER)
+                    .put("/" + idUser)
                 .then()
                     .statusCode(HttpStatus.SC_OK)
                     .log().all();

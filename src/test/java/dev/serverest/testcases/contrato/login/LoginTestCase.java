7f9bfa85551f5deb7921e7fb0bcc57790e656374
Luciano <lof.fagundes@gmail.com>
2025-04-03 16:53:50 -0300

    criei uma pasta de contrato. E testei o login foi efetuado, e retronou 200 e o token foi armazenado

--- src/test/java/dev/serverest/testcases/contrato/login/LoginTestCase.java
+++ src/test/java/dev/serverest/testcases/contrato/login/LoginTestCase.java
@@ -1,4 +1,30 @@
-package dev.serverest.testcases.contracts.login;
+package dev.serverest.testcases.contrato.login;
 
-public class LoginTestCase {
+import dev.serverest.bases.LoginBaseTest;
+import dev.serverest.managers.PropertiesManager;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.restassured.RestAssured;
+import org.apache.http.HttpStatus;
+import org.testng.annotations.Test;
+
+public class LoginTestCase extends LoginBaseTest {
+
+    @Epic("Teste de Login")
+    @Feature("[POST] - Teste para fazer login.")
+    @Description("Deve retornar 200 e confirmar que o usuário foi logado.")
+    @Test(priority = 1)
+    public void efeturaLogin200() {
+      auth =
+        RestAssured.given()
+                    .spec(loginRequest)
+                .when()
+                    .post()
+                .then()
+                    .statusCode(HttpStatus.SC_OK)
+                    .log().all()
+                .extract().path("authorization");
+        PropertiesManager.setProperty("authorize", "auth", "AUTH", auth);
+    }
 }

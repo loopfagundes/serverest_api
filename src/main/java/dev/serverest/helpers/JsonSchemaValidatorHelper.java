38d5e4c252f16161ea35ea07921962e6e4e77819
Luciano <lof.fagundes@gmail.com>
2025-04-03 16:57:09 -0300

    feat: adicionei uma classe para validar no arquivo de json

--- src/main/java/dev/serverest/helpers/JsonSchemaValidatorHelper.java
+++ src/main/java/dev/serverest/helpers/JsonSchemaValidatorHelper.java
@@ -1,11 +1,11 @@
-package dev.serverest.utils;
+package dev.serverest.helpers;
 
 import io.restassured.module.jsv.JsonSchemaValidator;
 
-public class JsonSchemaValidatorHelper  {
-    private static final String SCHEMAS = "schemas/";
+public class JsonSchemaValidatorHelper {
+    private static final String PATH_SCHEMAS = "schemas/";
 
-    public static void json(String path) {
-        JsonSchemaValidator.matchesJsonSchemaInClasspath(SCHEMAS + path);
+    public static JsonSchemaValidator validateJson(String pathToSchema) {
+        return JsonSchemaValidator.matchesJsonSchemaInClasspath(PATH_SCHEMAS + pathToSchema + ".json");
     }
 }

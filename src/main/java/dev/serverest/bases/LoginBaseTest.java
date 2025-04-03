2cbc620e60b2e797b8305c1dbcc51e06c5ea9663
Luciano <lof.fagundes@gmail.com>
2025-04-03 16:40:04 -0300

    feat: adicionei uma classe para fazer login Request

--- src/main/java/dev/serverest/bases/LoginBaseTest.java
+++ src/main/java/dev/serverest/bases/LoginBaseTest.java
@@ -1,4 +1,27 @@
 package dev.serverest.bases;
 
-public class LoginBaseTest {
+import dev.serverest.endponits.EndpointConfig;
+import dev.serverest.stubs.LoginStub;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeClass;
+
+public class LoginBaseTest extends EndpointConfig {
+    public static RequestSpecification loginRequest;
+    protected static String auth;
+
+    @BeforeClass
+    public void setUp() {
+        login();
+    }
+
+    private void login() {
+        loginRequest = new RequestSpecBuilder()
+                .setBaseUri(BASE_URI)
+                .setBasePath(PATH_LOGIN)
+                .setBody(LoginStub.loginUsuario())
+                .setContentType(ContentType.JSON)
+                .build();
+    }
 }

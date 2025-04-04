861a723a2eb92c713062346999a2d3273ee779fc
Luciano <lof.fagundes@gmail.com>
2025-04-04 16:04:52 -0300

    renomeei a chave AUTH para TOKEN

--- src/main/java/dev/serverest/endponits/EndpointConfig.java
+++ src/main/java/dev/serverest/endponits/EndpointConfig.java
@@ -10,7 +10,7 @@ public class EndpointConfig {
     private static final Properties properties = PropertiesManager.loadProperties(DEFAULT_FILE, DEFAULT_FOLDER);
 
     protected static final String ID_USER = PropertiesManager.loadProperties("authorize", "idUser").getProperty("ID_USER");
-    protected static final String AUTH_USER = PropertiesManager.loadProperties("authorize", "auth").getProperty("AUTH");
+    protected static final String AUTH_USER = PropertiesManager.loadProperties("authorize", "auth").getProperty("TOKEN");
 
     protected static final String BASE_URI = properties.getProperty("BASE_URI");
     protected static final String PATH_USUARIOS = properties.getProperty("PATH_USUARIOS");

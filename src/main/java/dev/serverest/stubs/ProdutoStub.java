7794d97cfb26cf23b9972492cb2361b4041da9ff
Luciano <lof.fagundes@gmail.com>
2025-04-10 16:18:09 -0300

    adicionei um metodo para editar

--- src/main/java/dev/serverest/stubs/ProdutoStub.java
+++ src/main/java/dev/serverest/stubs/ProdutoStub.java
@@ -3,12 +3,19 @@ package dev.serverest.stubs;
 import dev.serverest.dto.ProdutoDTO;
 
 public class ProdutoStub {
-    public static ProdutoDTO cadastrarProdutoDto() {
+    public static ProdutoDTO cadastrarProdutoStub() {
         return new ProdutoDTO(
-            "LG Ultragear 24 180hz",
-            "1000",
-            "Monitor",
-            "200"
-        );
+                "LG Ultragear 24 180hz",
+                "1000",
+                "Monitor Gamer",
+                "200");
+    }
+
+    public static ProdutoDTO editarProdutoStub() {
+        return new ProdutoDTO(
+                "LG Ultragear 24 180hz - PROMOÇÂO 50%",
+                "500",
+                "Monitor Gamer",
+                "200");
     }
 }

ccffe741645781b8fd6fd9ebcedb354a9006e17b
Luciano <lof.fagundes@gmail.com>
2025-04-14 17:57:44 -0300

    anotacoes nao é mais necessarios

--- src/main/java/dev/serverest/dto/CarrinhoDTO.java
+++ src/main/java/dev/serverest/dto/CarrinhoDTO.java
@@ -1,16 +1,12 @@
 package dev.serverest.dto;
 
 import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
-@JsonPropertyOrder({"produtos"})
 public class CarrinhoDTO {
     private List<ProdutoNoCarrinhoDTO> produtos;
 }
